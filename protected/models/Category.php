f3d292adcc9624b779955931d2f24249e46470ad
bigjey <mail.bigjey@gmail.com>
2013-03-28 17:57:39 +0200

    file upload through model

--- protected/models/Category.php
+++ protected/models/Category.php
@@ -40,8 +40,8 @@ class Category extends CActiveRecord
 		return array(
 			array('name', 'required'),
 			array('name, logo', 'length', 'max'=>255),
-			array('logo', 'file', 'types'=>'gif, png, jpg', 'allowEmpty'=>true),
-			array('id, title', 'safe', 'on'=>'search'),
+			array('logo_file', 'file', 'types'=>'gif, png, jpg', 'allowEmpty'=>true, 'on'=>'update, add'),
+			array('id, name', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -86,4 +86,44 @@ class Category extends CActiveRecord
 			'criteria'=>$criteria,
 		));
 	}
+
+    protected function beforeSave(){
+        if(!parent::beforeSave())
+            return false;
+
+        if(($this->scenario=='insert' || $this->scenario=='update') &&
+            ($logo_file=CUploadedFile::getInstance($this,'logo_file'))){
+            $this->deleteFile();
+
+            $this->logo=$logo_file;
+            $this->logo->saveAs(
+                Yii::getPathOfAlias('webroot.uploads').DIRECTORY_SEPARATOR."original/".$this->logo);
+
+            $ih = new CImageHandler();
+            $ih->load(Yii::getPathOfAlias('webroot.uploads').DIRECTORY_SEPARATOR."original/".$this->logo)
+                ->adaptiveThumb(50, 50)
+                ->save(Yii::getPathOfAlias('webroot.uploads').DIRECTORY_SEPARATOR."thumb_50x50/".$this->logo);
+        }
+        return true;
+    }
+
+    protected function beforeDelete(){
+        if(!parent::beforeDelete())
+            return false;
+        $this->deleteFile(); // удалили модель? удаляем и файл
+        return true;
+    }
+
+    public function deleteFile(){
+        $path=Yii::getPathOfAlias('webroot.uploads').DIRECTORY_SEPARATOR."original/".
+            $this->logo;
+        if(is_file($path))
+            unlink($path);
+
+        $path=Yii::getPathOfAlias('webroot.uploads').DIRECTORY_SEPARATOR."thumb_50x50/".
+            $this->logo;
+        if(is_file($path))
+            unlink($path);
+    }
+
 }
